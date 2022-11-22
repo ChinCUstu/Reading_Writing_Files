af39a5eb6b56bb78b2cfc2ac7128f23719b495ee
ChinCUstu <ndulue.chinedu@stu.cu.edu.ng>
2022-11-22 17:04:58 +0100

    1. Personal Information:                      Write a program that displays the following information: • Your name • Your address, with city, state, and ZIP • Your telephone number • Your college major

---
+++ Personal Information.py
@@ -0,0 +1,9 @@
+name = 'Fatum Castillo'
+address = '31 Rue Adolphe Mille ,Apt. 551, 81801 Vitry-sur-Seine, Rhône-Alpes France'
+telephone_number = '+33 03.67.24.72.44'
+college_major = 'Science of Decision'
+
+print('name: ', name)
+print('address: ', address)
+print('Telephone number: ', telephone_number)
+print('College Major', college_major)
