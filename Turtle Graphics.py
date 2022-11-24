a01a4951a7b4a15a390a4eea00e0fcf6435ff67d
ChinCUstu <ndulue.chinedu@stu.cu.edu.ng>
2022-11-24 13:49:03 +0100

    Turtle Graphics for beginners. Joint slanted squares and a small triangle in a big triangle Cube, Tangled circles...... Turtle Graphics.py update.....

--- Turtle Graphics.py
+++ Turtle Graphics.py
@@ -2,17 +2,17 @@ import turtle
 
 import square
 
-turtle.speed(5)
+turtle.speed(20)
 turtle.hideturtle()
-square.square()
-square.half_square()
-square.square()
+square.slated_square()
+square.half_slated_square()
+square.slated_square()
 
 turtle.clear()
 
 turtle.reset()
 turtle.hideturtle()
-square.half_square()
+square.half_slated_square()
 turtle.setheading(180)
 turtle.forward(107)
 
@@ -20,4 +20,13 @@ turtle.setheading(65)
 turtle.forward(126)
 turtle.setheading(295.5)
 turtle.forward(128)
+
+turtle.clear()
+
+square.cube()
+
+turtle.clear()
+turtle.reset()
+
+square.tangled()
 turtle.done()
--- square.py
+++ square.py
@@ -1,19 +1,87 @@
 import turtle
 
 
+def slated_square():
+    turtle.setheading(45)
+    turtle.forward(75)
+    turtle.setheading(315)
+    turtle.forward(75)
+    turtle.right(90)
+    turtle.forward(75)
+    turtle.right(90)
+    turtle.forward(75)
+
+
+def half_slated_square():
+    turtle.setheading(45)
+    turtle.forward(75)
+    turtle.setheading(315)
+    turtle.forward(75)
+
+
 def square():
-    turtle.setheading(45)
+    turtle.setheading(90)
     turtle.forward(75)
-    turtle.setheading(315)
+    turtle.setheading(180)
     turtle.forward(75)
-    turtle.right(90)
+    turtle.setheading(270)
     turtle.forward(75)
-    turtle.right(90)
+    turtle.setheading(0)
     turtle.forward(75)
 
 
-def half_square():
-    turtle.setheading(45)
+def reversed_square():
     turtle.forward(75)
+    turtle.setheading(270)
+    turtle.forward(75)
+    turtle.setheading(180)
+    turtle.forward(75)
+    turtle.setheading(90)
+    turtle.forward(75)
+
+
+def cube():
+    turtle.reset()
+    turtle.hideturtle()
+    square()
+    reversed_square()
+    turtle.left(45)
+    turtle.forward(105)
+    turtle.goto(0, 0)
     turtle.setheading(315)
-    turtle.forward(75)
+    turtle.forward(105)
+    turtle.penup()
+    turtle.goto(0, -75)
+    turtle.pendown()
+    turtle.setheading(135)
+    turtle.forward(105)
+    turtle.penup()
+    turtle.goto(0, 75)
+    turtle.setheading(315)
+    turtle.pendown()
+    turtle.forward(105)
+
+
+def sets():
+    turtle.circle(25)
+    turtle.right(45)
+    turtle.penup()
+    turtle.forward(20)
+    turtle.pendown()
+    turtle.circle(25)
+
+
+def tangled():
+    sets()
+    turtle.penup()
+    turtle.goto(0, 0)
+    turtle.setheading(0)
+    turtle.forward(65)
+    turtle.pendown()
+    sets()
+    turtle.penup()
+    turtle.goto(72, 0)
+    turtle.setheading(0)
+    turtle.forward(59)
+    turtle.pendown()
+    sets()
